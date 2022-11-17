29ebed1401be71ca9ab3c4906a3bdc77fc8f627d
Locit2003 <110243167+Locit2003@users.noreply.github.com>
2022-11-17 10:20:41 +0700

    loginUser

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,25 +1,48 @@
-import { Body, Controller, Get, HttpCode, Post, Delete, UsePipes, Param, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Delete, UsePipes, Param, ValidationPipe, Patch } from '@nestjs/common';
 import { createUserDto } from './dto/createUser.dto';
+import { updateUserDto } from './dto/updateUser.dto';
 import { userService } from './user.service';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+import { loginUserDto } from './dto/loginUser.dto';
 
 @Controller('User')
 export class UserController {
     constructor(private userService: userService){}
-    @Get('/list')
+    @Get('/')
     getUsers(){
         return this.userService.findAll();
     }
 
-    @Post('/create')
+    @Post('/')
     @HttpCode(200)
     @UsePipes(ValidationPipe)
-    async createUser(@Body() UserCreate: createUserDto){
-        return await this.userService.create(UserCreate);
+    async register(@Body() UserCreate: createUserDto){
+        return this.userService.create(UserCreate)
     }
 
-    @Delete('/delete/:id')
+    @Delete(':id')
     remove(@Param('id') id: number){
         return this.userService.remove(+id);
+    }
+
+    @Patch(':id')
+    updateUser(@Param('id') id:number, @Body() updateUserDto:updateUserDto){
+        return this.userService.update(+id, updateUserDto);
+    }
+    //lấy theo id 
+    @Get(':id')
+    findOne(@Param('id') id:object ){
+        const searchById = this.userService.findOne(id);
+        if(searchById) {
+            return searchById;
+          }else{
+            return 'không có người bình trọn này';
+          }
+    }
+
+    @Post('/login')
+    login(@Body() info:loginUserDto){
+        return this.userService.login(info)
     }
 }
--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -4,6 +4,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { createUserDto } from './dto/createUser.dto';
 import { updateUserDto } from './dto/updateUser.dto';
+import { loginUserDto } from './dto/loginUser.dto';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class userService {
@@ -16,19 +19,48 @@ export class userService {
     return await this.UserRepo.find();
   } 
 
-  findOne(id: number): Promise<User> {
-    return this.UserRepo.findOneBy({ id });
-  }git 
+  async findOne(id?: object): Promise<loginUserDto> {
+    const userDto = await this.UserRepo.findOne(id)
+    return userDto;
+  }
 
   async create(data: createUserDto){
-    return await this.UserRepo.save(data)
+    data.password =  await bcrypt.hash(data.password, 10);
+    try {
+      const user_bcrypt = await this.UserRepo.save(data);
+      delete user_bcrypt.password;
+      return user_bcrypt;
+    } catch (error) {
+      throw new Error('User already exists');
+    }
   }
 
   remove(id: number){
     return this.UserRepo.delete(id)
   }
 
-  update(id: number, userUpdateDto:updateUserDto){
-    return this.UserRepo.update(+id, userUpdateDto);
+  async update(id: number, userUpdateDto:updateUserDto){
+    return await this.UserRepo.update(+id, userUpdateDto);
+  }
+
+  async login(Dto: loginUserDto){
+    // tìm kiếm user theo email
+    const user = await this.UserRepo.findOne({
+      where : {
+        email: Dto.email,
+      }
+    });
+    if(!user){
+      throw new Error(`thông tin tài khoản không chính xác`);
+    }
+
+    const pw = await bcrypt.compare(Dto.password, user.password);
+    if(!pw){
+      throw new Error('thông tin tài không chính xác')
+    }    
+
+    delete user.password;
+    return user;
+    // tìm kiếm user theo password
   }
 }
